9b142186be7dfdffa9db7daa41965e5a09e17500
MatiAquino0 <matiaquino@uade.edu.ar>
2025-04-26 17:06:00 -0300

    Actualizacion usuario

--- carga_de_informacion/__init__.py
+++ carga_de_informacion/__init__.py
@@ -2,4 +2,5 @@ from .productos import cargar_productos
 from .clientes import cargar_clientes
 from .compras import Registrar_compras
 from .ventas import cargar_ventas
+from .usuario import carga_usuario
 import re
